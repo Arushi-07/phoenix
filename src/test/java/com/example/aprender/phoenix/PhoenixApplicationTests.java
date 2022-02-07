baf62cec01d8060936f5f35f0c482da7a2714ccb
Arushi-07 <arushi7696@gmail.com>
2022-02-07 16:30:26 -0700

    initial commit

---
+++ src/test/java/com/example/aprender/phoenix/PhoenixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aprender.phoenix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoenixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
